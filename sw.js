5173ee1ea42a4d781b57e02c28e7d48c48a3b242
lihengdong <admin@lihengdong.com>
2019-08-18 22:45:54 +0800

    Site updated: 2019-08-18 22:45:52

--- sw.js
+++ sw.js
@@ -1,18 +1,3 @@
-"use strict";
-(function() {
-  var cacheVersion = "-180503";
-  var staticCacheName = "asset" + cacheVersion;
-  var maxEntries = 100;
-  self.importScripts("https://cdn.jsdelivr.net/npm/sw-toolbox@3.6.0/sw-toolbox.js");
-
-  self.toolbox.options.debug = false;
-  self.toolbox.options.networkTimeoutSeconds = 1;
-
-  /* staticImageCache */
-  self.toolbox.router.get("/(.*)", self.toolbox.cacheFirst, {
-    cache: {
-      name: staticCacheName,
-      maxEntries: maxEntries
-    }
-  });
-})();
+// build time:Sun Aug 18 2019 22:45:52 GMT+0800 (GMT+08:00)
+"use strict";(function(){var o="-180503";var t="asset"+o;var s=100;self.importScripts("https://cdn.jsdelivr.net/npm/sw-toolbox@3.6.0/sw-toolbox.js");self.toolbox.options.debug=false;self.toolbox.options.networkTimeoutSeconds=1;self.toolbox.router.get("/(.*)",self.toolbox.cacheFirst,{cache:{name:t,maxEntries:s}})})();
+//rebuild by neat 
