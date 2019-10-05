158ff7316cda9a053fb1b55e1dc13285351ac48f
lihengdong <admin@lihengdong.com>
2019-10-05 20:58:38 +0800

    Site updated: 2019-10-05 20:58:34

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// build time:Fri Oct 04 2019 22:58:57 GMT+0800 (GMT+08:00)
+// build time:Sat Oct 05 2019 20:58:32 GMT+0800 (GMT+08:00)
 "use strict";(function(){var o="-180503";var t="asset"+o;var s=100;self.importScripts("https://cdn.jsdelivr.net/npm/sw-toolbox@3.6.0/sw-toolbox.js");self.toolbox.options.debug=false;self.toolbox.options.networkTimeoutSeconds=1;self.toolbox.router.get("/(.*)",self.toolbox.cacheFirst,{cache:{name:t,maxEntries:s}})})();
 //rebuild by neat 
